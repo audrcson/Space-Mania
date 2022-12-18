8b190a33d34e79f2b5d197f92729da8e5f184018
ajiesaii <118529191+ajiesaii@users.noreply.github.com>
2022-12-18 14:11:59 +0700

    Add files via upload

---
+++ SPACE_SHOOTER_GAME/src/PBO/Main.java
@@ -0,0 +1,11 @@
+package PBO;
+
+import mainWindow.MainFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainFrame mf = new MainFrame();
+    }
+}
---
+++ SPACE_SHOOTER_GAME/src/mainWindow/MainFrame.java
@@ -0,0 +1,17 @@
+package mainWindow;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        init();
+    }
+
+    public void init(){
+        setVisible(true);
+        setResizable(false);
+        setTitle("SPACE SHOOTER GAME");
+        setLocationRelativeTo(null);
+
+    }
+}
